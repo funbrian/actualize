31e737e7e0d7ed98502b5e0eb44f8a4a8566cd5b
Brian <brianpdevane@gmail.com>
2020-02-25 19:34:31 -0500

    add store item

--- employee.rb
+++ employee.rb
@@ -1,8 +1,12 @@
-employee = ["Majora", "Carter", 80000, true]
+employee1 = ["Majora", "Carter", 80000, true]
 
 
-p employee[0] + " " + employee[1] + " makes $" + employee[2].to_s + " per year."
+p employee1[0] + " " + employee1[1] + " makes $" + employee1[2].to_s + " per year."
 
 employee2 = ["Danilo", "Campos", 70000, false]
 
 p "#{employee2[0]} #{employee2[1]} makes $#{employee2[2]} per year."
+
+employee3 = {first_name: "Danilo", last_name: "Campos", salary: 80000, active: true}
+
+p "#{employee3[:first_name]} #{employee3[:last_name]} makes $#{employee3[:salary]} per year."
---
+++ object_oriented_ruby/store_item.rb
@@ -0,0 +1,6 @@
+banana = {color: "yellow", price: 2.99, category: "fruit"}
+apple = {"color" => "red", "price" => 1.99, "category" => "fruit"}
+spinach = {color: "green", price: 3.99, category: "vegetable"}
+p banana
+p apple
+p spinach
